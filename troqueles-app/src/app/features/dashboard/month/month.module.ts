e9e71bd7a34bfdd72a86c44eb386409717870c09
pellegrineschi <pellegrineschi86@gmail.com>
2024-10-10 21:22:48 -0300

    creacion de modulos

---
+++ troqueles-app/src/app/features/dashboard/month/month.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MonthRoutingModule } from './month-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MonthRoutingModule
+  ]
+})
+export class MonthModule { }
---
+++ troqueles-app/src/app/features/dashboard/troquel/troquel.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TroquelRoutingModule } from './troquel-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TroquelRoutingModule
+  ]
+})
+export class TroquelModule { }
